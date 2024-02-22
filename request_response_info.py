86499e8c514ddd1e4a071236c132b71896e030ef
PaulinaKimak <p.kimak.047@studms.ug.edu.pl>
2024-02-22 20:08:28 +0100

    'RockPiryt' - add 2 routes: books and book/id

--- request_response_info.py
+++ request_response_info.py
@@ -5,18 +5,24 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     ###  Request info
-     
+    
+    #  różne headery
     #  print(request.headers)
     #  print(f'method:{request.method}')
     #  print(f'path:{request.path}')
     #  print(f'url:{request.url}')
     #  print(request.headers['Authorization'])
     #  print(request.headers['Content-Type'])
-    #  print(request.json) - wyświetla body requesta
+    
+    #  wyświetla body requesta
+    #  print(request.json) 
     #  print(request.json['name'])
     
     ### Response info
+    # zwykła odpowiedź
     # response = jsonify([{'id': 1, 'title': 'Title XXX'}])
+    
+    # zmiana kodu odpowiedzi
     response = jsonify({'error': 'Not Found'})
     response.status_code = 404
     return response
